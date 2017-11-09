b2a513200c1c077596e63aa430f688e7f871f451
Ryan Tibbetts <ryantibbetts@Ryans-Mac-mini.local>
2017-11-09 15:23:27 -0500

    script4.js complete, Lesson on making a server.

---
+++ script4.js
@@ -0,0 +1,11 @@
+var server = require('http'); //http is a module
+
+server.createServer(engine).listen(1337) //leets
+
+function engine(request, response) {
+
+  response.writeHead(200,{'Content-Type' : 'text/plain'});
+
+  response.end('Hey there from the server! :D');
+
+}
