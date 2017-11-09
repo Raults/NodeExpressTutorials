11b904cb8a57bd7a0167377daee1ef403cb06f7c
Ryan Tibbetts <ryantibbetts@Ryans-Mac-mini.local>
2017-11-09 15:44:35 -0500

    script5.js completed. Learned about basics of requests and responses.

---
+++ script5.js
@@ -0,0 +1,16 @@
+var http = require('http'); //http is a module
+var server = http.createServer(engine);
+
+server.listen(1337, function() {
+  console.log('Server was hit by a reqest');
+}); //leets
+
+function engine(request, response) {
+
+//console.log(response);
+
+  response.writeHead(200,{'Content-Type' : 'text/plain'});
+
+  response.end('Hey there from the server! :D. Request from page ' +request.url);
+
+}
