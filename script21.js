b26ba399af1dbfbfabde49a4a2683c65add2ca9a
Ryan Tibbetts <ryantibbetts@Ryans-Mac-mini.local>
2017-11-14 13:19:28 -0500

    script21.js completed. in this tutorial we went over cookies.

---
+++ script21.js
@@ -0,0 +1,14 @@
+var express = require('express');
+var cookieParser = require('cookie-parser');
+
+var app = express();
+app.use(cookieParser());
+
+app.get('/', function(req, resp) {
+  resp.cookie('myFirstCookie', 'looks Good'/* look upp documentation for features*/)
+  resp.end('Wow');
+});
+
+app.listen(1337. function() {
+  console.log('Yeah?');
+})
